2ade5387b70d4a5db282826fdf496e3b31aad0a4
JacenWilliams <jacencwilliams@gmail.com>
2019-11-19 14:21:51 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')();
+const http = require('http').createServer(express);
+const io = require('socket.io')(http);
+
+io.on('connection', function(socket) {
+    socket.on('message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit(message);
+    })
+});
+
+http.listen(80, function() {
+    console.log('listening on port: ' + 80);
+});
