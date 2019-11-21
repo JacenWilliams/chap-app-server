968ed45880d2fcc9a7aea9935382cb7c352d4458
JacenWilliams <jacencwilliams@gmail.com>
2019-11-20 18:43:12 -0600

    anakin.jpg

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express')();
-const http = require('http').createServer(express);
+const express = require('express');
+let app = express();
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
+let port = process.env.PORT || 3000;
 
 io.on('connection', function(socket) {
+    console.log('connected to :' + socket);
     socket.on('message', function(msg) {
         console.log('message: ' + msg);
-        io.emit(message);
+        io.emit('message', msg);
     })
 });
 
-http.listen(80, function() {
-    console.log('listening on port: ' + 80);
-});  
+http.listen(port, function() {
+    console.log('listening on port: ' + port);
+});
